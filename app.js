caaa440d5725e2146dabf76309ad35a6be78789d
hadashi <dev@hadashikick.jp>
2012-09-30 00:30:41 +0900

    デバッグモードで動くようになった

--- app.js
+++ app.js
@@ -4,10 +4,19 @@
  */
 
 var express = require('express')
-  , http = require('http')
-  , path = require('path');
+  , http    = require('http')
+  , path    = require('path');
 
-var app = express();
+
+var app = express()
+  , server = http.createServer(app)
+  , io  = require('socket.io').listen(server);
+
+io.sockets.on('connection', function(socket) {
+  socket.on('accelerate', function(accel) {
+    socket.broadcast.volatile.json.emit('move', accel);
+  });
+});
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
@@ -29,6 +38,6 @@ app.get('/', function(req, res) {
   res.render('index');
 });
 
-http.createServer(app).listen(app.get('port'), function(){
+server.listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
 });
